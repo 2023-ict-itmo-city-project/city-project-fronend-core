03c9bd6fb3fe4054776cc3dca46c90af21e4ae78
LiprikON2 <44316968+LiprikON2@users.noreply.github.com>
2023-11-04 15:29:47 +0300

    feat: add routing

---
+++ city-24/src/Root.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link, Outlet } from "react-router-dom";
+import { AppShell, Burger } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+
+import "./Root.css";
+
+const App = () => {
+    const [opened, { toggle }] = useDisclosure();
+    return (
+        <AppShell
+            header={{ height: 60 }}
+            navbar={{ width: 300, breakpoint: "sm", collapsed: { mobile: !opened } }}
+            padding="md"
+        >
+            <AppShell.Header>
+                <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+                <div>Logo</div>
+            </AppShell.Header>
+
+            <AppShell.Navbar p="md">
+                <Link to="/">Home</Link>
+                <Link to="/reports">Reports</Link>
+            </AppShell.Navbar>
+
+            <AppShell.Main>
+                <Outlet />
+            </AppShell.Main>
+        </AppShell>
+    );
+};
+
+export default App;
--- city-24/src/main.jsx
+++ city-24/src/main.jsx
@@ -1,17 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ColorSchemeScript, MantineProvider } from "@mantine/core";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import "@mantine/core/styles.layer.css";
 
-import App from "./App.jsx";
 import { theme } from "./theme";
+import { Home } from "./scenes";
+import Root from "./Root";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root />,
+        children: [
+            {
+                path: "/",
+                element: <Home />,
+            },
+            {
+                path: "/reports",
+                element: <div>There are no reports</div>,
+            },
+        ],
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <ColorSchemeScript defaultColorScheme="auto" />
         <MantineProvider defaultColorScheme="auto" theme={theme}>
-            <App />
+            <RouterProvider router={router} />
         </MantineProvider>
     </React.StrictMode>
 );
